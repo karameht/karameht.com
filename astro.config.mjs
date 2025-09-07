d3c6acfbe580518b9745016b0d455569234558f2
karameht <dev@bykuz.de>
2025-09-08 00:29:49 +0200

    chore(init): setup astro project with tooling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://karameht.com',
+  integrations: [mdx()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
